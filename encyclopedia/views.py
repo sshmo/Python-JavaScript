ce8211f085b6bdf757693574dec9d9fdae613566
sshmo <nabaeazim1@gmail.com>
2020-10-17 13:45:31 +0330

    Specification 1,2,6 are ok

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,27 @@
+import random
 from django.shortcuts import render
 
 from . import util
 
 
 def index(request):
+
+    random_title = random.choice(util.list_entries())
+
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "random_title":random_title
     })
 
+
+def entry(request, title):
+
+    random_title = random.choice(util.list_entries())
+
+    content = util.get_entry(title)
+    
+    return render(request, "encyclopedia/entry.html", {
+        "content": content,
+        "title" : title if content else "Error",
+        "random_title":random_title
+    })
