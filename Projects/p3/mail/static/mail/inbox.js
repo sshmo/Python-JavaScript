d8bb578d1ba3531a3a850ea789a5ff00e114ad92
sshmo <nabaeazim1@gmail.com>
2020-11-21 17:22:42 +0330

    View Email is working

--- Projects/p3/mail/static/mail/inbox.js
+++ Projects/p3/mail/static/mail/inbox.js
@@ -17,6 +17,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function compose_email() {
   // Show compose view and hide other views
+  document.querySelector("#email-view").style.display = "none";
   document.querySelector("#emails-view").style.display = "none";
   document.querySelector("#compose-view").style.display = "block";
 
@@ -57,6 +58,7 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector("#emails-view").innerHTML = "";
   document.querySelector("#emails-view").style.display = "block";
+  document.querySelector("#email-view").style.display = "none";
   document.querySelector("#compose-view").style.display = "none";
 
   const container = document.createElement("div");
@@ -73,13 +75,17 @@ function load_mailbox(mailbox) {
     .then((emails) => {
       emails.forEach((email) => {
         const row = document.createElement("div");
-        row.className =
-          "row border rounded my-1 align-text-middle text-dark" +
-          (email["read"] ? "bg-secondary" : "");
-        row.style = "width:80%";
+
+        row.className = `row border rounded my-1 align-text-middle text-dark`;
+
+        const bgc = !email["read"]
+          ? " background-color: white;"
+          : " background-color: #E8E8E8;";
+        row.style = `width:80%; ${bgc};`;
 
         container.appendChild(row);
 
+        // Add click listener to each email
         row.addEventListener("click", function () {
           load_email(email["id"]);
         });
@@ -103,9 +109,68 @@ function load_mailbox(mailbox) {
     });
 }
 
-function load_email(email) {
-  
+function load_email(email_id) {
   // Show the email and hide other views
-  console.log(`Email number ${email}loaded!`);
+  document.querySelector("#compose-view").style.display = "none";
+  document.querySelector("#emails-view").style.display = "none";
+  document.querySelector("#email-view").innerHTML = "";
+  document.querySelector("#email-view").style.display = "block";
 
+  // Mark email as read
+  fetch(`/emails/${email_id}`, {
+    method: "PUT",
+    body: JSON.stringify({
+      read: true,
+    }),
+  });
+      
+  const container = document.createElement("div");
+  container.className = "container w-75";
+
+  // Show the email
+  fetch(`/emails/${email_id}`)
+    .then((response) => response.json())
+    .then((email) => {
+      
+      const row_1 = document.createElement("div");
+      row_1.className = `row border rounded my-1 align-text-middle text-dark`;
+      container.appendChild(row_1);
+
+      const col_1_1 = document.createElement("div");
+      col_1_1.className = "col";
+      row_1.appendChild(col_1_1);
+      col_1_1.innerHTML = `Subject: ${email["subject"]}`;
+
+      const row_2 = document.createElement("div");
+      row_2.className = `row border rounded my-1 align-text-middle text-dark`;
+      container.appendChild(row_2);
+
+      const col_2_1 = document.createElement("div");
+      col_2_1.className = "col";
+      row_2.appendChild(col_2_1);
+      col_2_1.innerHTML = `From: ${email["sender"]}`;
+
+      const col_2_2 = document.createElement("div");
+      col_2_2.className = "col";
+      row_2.appendChild(col_2_2);
+      col_2_2.innerHTML = `To: ${email["recipients"]}`;
+
+      const col_2_3 = document.createElement("div");
+      col_2_3.className = "col";
+      row_2.appendChild(col_2_3);
+      col_2_3.innerHTML = `On: ${email["timestamp"]}`;
+
+
+      const row_3 = document.createElement("div");
+      row_3.className = `row border rounded my-1 align-text-middle text-dark`;
+      container.appendChild(row_3);
+
+      const col_3_1 = document.createElement("div");
+      col_3_1.className = "col h-100px";
+      row_3.appendChild(col_3_1);
+      col_3_1.innerHTML = `${email["body"]}`;
+
+
+      document.querySelector("#email-view").append(container);
+    });
 }
